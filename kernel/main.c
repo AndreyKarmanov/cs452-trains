b2790a779e9f526799e508204f0766802b961c4e
Andrey Karmanov <andrey.karmanov@uwaterloo.ca>
2026-05-26 17:57:07 -0400

    task stacks in separate memory region

--- kernel/main.c
+++ kernel/main.c
@@ -5,18 +5,59 @@
 #include "rpi.h"
 #include "uart.h"
 
+#define TASK_STACK_SIZE 4096
+#define TASK_DESCRIPTORS 4
+
 extern "C" void setup_mmu(); // in mmu.S
+
+// everything that we need to save to account for context-switch
+struct TrapFrame
+{
+	// need to save x0 to x30 to allow for context switching
+	uint64_t x[31];
+
+	// also have to save the SP_EL0 and ELR_EL1 registers to allow for context switching
+	uint64_t sp_el0; // this is the stack pointer
+	uint64_t elr_el1; // this is where to jump after kernel code is done
+	uint64_t spsr_el1; // Program stat, including condition codes, interrupt flags, execution state, etc. 
+};
+
+enum class TaskState
+{
+	READY,
+	RUNNING,
+	TERMINATED
+};
+
+struct TaskDescriptor
+{
+	int tid;
+	int parent_tid;
+	int priority;
+	TaskState state;
+	TrapFrame tf;
+	uint8_t* stack_base;
+	size_t stack_size;
+};
+
+// This can live in the data section alongside other kernel data
+TaskDescriptor task_descriptors[TASK_DESCRIPTORS];
+
+// Make sure this lives in a separate, non-kernel section
+uint8_t task_stacks[TASK_DESCRIPTORS][TASK_STACK_SIZE] __attribute__((section(".task_stacks")));
 
 extern "C" int kmain() {
 #if defined(MMU)
 	setup_mmu();
 #endif
-	// set up GPIO pins for both console uart and canbus
 	gpio_init();
-	// not strictly necessary, since console is configured during boot
 	uart_config_and_enable(CONSOLE);
 
 	uart_puts(CONSOLE, "\033[2J\033[?25l\033[1;1H" __DATE__ " / " __TIME__ " / Andrey Karmanov / Anthony Ho\n\r");
+
+	for (size_t i = 0; i < TASK_DESCRIPTORS; i++) {
+		uart_printf(CONSOLE, "Task %u stack: 0x%x\n\r", i, &task_stacks[i]);
+	}
 
 	for (;;) {
 
