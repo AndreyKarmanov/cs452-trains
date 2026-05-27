1183d1f6356567f799b39ca5b412f78d40d3de1d
Anthony Ho <anthony52ho@gmail.com>
2026-05-27 17:09:34 -0400

    non blocking i/o

--- kernel/shell.c
+++ kernel/shell.c
@@ -2,51 +2,79 @@
 #include <cstring>
 #include <ctype.h>
 
-#include "uart.h"
 #include "shell.h"
 #include "syscall.h"
+#include "uart_non_blocking.h"
 
 #define BUFFER_SIZE 32
 
-void fire_command(const char* buf, size_t blen) {
-    if (blen == 0) return;
-    if (strncmp(buf, "q", 1) == 0) {
-        Exit();
-    } else if (strncmp(buf, "p", 1) == 0) {
-        int parent_tid = MyParentTid();
-        uart_printf(CONSOLE, "My parent tid is %d\n\r", parent_tid);
-    } else if (strncmp(buf, "m", 1) == 0) {
-        int my_tid = MyTid();
-        uart_printf(CONSOLE, "My tid is %d\n\r", my_tid);
-    } else if (strncmp(buf, "y", 1) == 0) {
-        Yield();
-        uart_puts(CONSOLE, "Yielded\n\r");
-    } else if (strncmp(buf, "c", 1) == 0) {
-        int tid = Create(0, shell);
-        uart_printf(CONSOLE, "Created task with tid %d\n\r", tid);
-    } else {
-        uart_puts(CONSOLE, "Unknown command. Available: q (quit), p (parent tid), m (my tid), y (yield), c (create)\n\r");
-    }
+void fire_command(const char *buf, size_t blen, UARTNB &uart) {
+  if (blen == 0)
+    return;
+  if (strncmp(buf, "q", 1) == 0) {
+    Exit();
+  } else if (strncmp(buf, "p", 1) == 0) {
+    int parent_tid = MyParentTid();
+    uart.printf("My parent tid is %d\n\r", parent_tid);
+  } else if (strncmp(buf, "m", 1) == 0) {
+    int my_tid = MyTid();
+    uart.printf("My tid is %d\n\r", my_tid);
+  } else if (strncmp(buf, "y", 1) == 0) {
+    Yield();
+    uart.puts("Yielded\n\r");
+  } else if (strncmp(buf, "c", 1) == 0) {
+    int tid = Create(0, shell);
+    uart.printf("Created task with tid %d\n\r", tid);
+  } else {
+    uart.puts("Unknown command. Available: q (quit), p (parent tid), "
+              "m (my tid), y (yield), c (create)\n\r");
+  }
 }
 
 void shell() {
-    char buf[BUFFER_SIZE];
-    size_t buf_n = 0;
-    uart_puts(CONSOLE, "COMMANDS: q (quit) p (parent tid) m (my tid) y (yield) c (create)\n\r> ");
-    while (1) {
-        char c = uart_getc(CONSOLE);
-        if (isprint(c) && buf_n < BUFFER_SIZE - 1) {
-            buf[buf_n++] = c;
-            uart_putc(CONSOLE, c);
-        } else if ((c == 0x08 || c == 0x7f) && buf_n > 0) { // backspace
-            uart_puts(CONSOLE, "\b \b"); // move back, print space, move back again
-            --buf_n;
-        } else if (c == '\r') { // enter
-            buf[buf_n] = '\0';
-            uart_puts(CONSOLE, "\n\r");
-            fire_command(buf, buf_n);
-            uart_puts(CONSOLE, "> ");
-            buf_n = 0;
-        }
+  char buf[BUFFER_SIZE];
+  size_t buf_n = 0;
+  UARTNB uart(CONSOLE);
+  uart.puts("COMMANDS: q (quit) p (parent tid) m (my tid) y (yield) c "
+            "(create)\n\r> ");
+  while (1) {
+    if (uart.can_receive_io()) {
+      char c = uart.getc();
+      if (isprint(c) && buf_n < BUFFER_SIZE - 1) {
+        buf[buf_n++] = c;
+        uart.putc(c);
+      } else if ((c == 0x08 || c == 0x7f) && buf_n > 0) { // backspace
+        uart.puts("\b \b"); // move back, print space, move back again
+        --buf_n;
+      } else if (c == '\r') { // enter
+        buf[buf_n] = '\0';
+        uart.puts("\n\r");
+        fire_command(buf, buf_n, uart);
+        uart.puts("> ");
+        buf_n = 0;
+      }
     }
+    uart.send_io();
+  }
+  // char buf[BUFFER_SIZE];
+  // size_t buf_n = 0;
+  // uart_puts(CONSOLE, "COMMANDS: q (quit) p (parent tid) m (my tid) y (yield)
+  // c "
+  //                    "(create)\n\r> ");
+  // while (1) {
+  //   char c = uart_getc(CONSOLE);
+  //   if (isprint(c) && buf_n < BUFFER_SIZE - 1) {
+  //     buf[buf_n++] = c;
+  //     uart_putc(CONSOLE, c);
+  //   } else if ((c == 0x08 || c == 0x7f) && buf_n > 0) { // backspace
+  //     uart_puts(CONSOLE, "\b \b"); // move back, print space, move back again
+  //     --buf_n;
+  //   } else if (c == '\r') { // enter
+  //     buf[buf_n] = '\0';
+  //     uart_puts(CONSOLE, "\n\r");
+  //     fire_command(buf, buf_n);
+  //     uart_puts(CONSOLE, "> ");
+  //     buf_n = 0;
+  //   }
+  // }
 }
--- kernel/uart_non_blocking.c
+++ kernel/uart_non_blocking.c
@@ -1,5 +1,6 @@
-#include "uart.h"
+#include "uart_non_blocking.h"
 #include "rpi.h"
+#include "uart.h"
 #include "util.h"
 #include <stdarg.h>
 #include <stdint.h>
@@ -38,89 +39,93 @@ static const uint32_t UART_LCRH_FEN       = 0x10;
 static const uint32_t UART_LCRH_WLEN_LOW  = 0x20;
 static const uint32_t UART_LCRH_WLEN_HIGH = 0x40;
 
-// // Configure the line properties (e.g, parity, baud rate) of a UART and
-// ensure that it is enabled void uart_config_and_enable(size_t line) {
-// uint32_t baud_ival, baud_fval; 	uint32_t flag = UART_LCRH_FEN;
+// Configure the line properties (e.g, parity, baud rate) of a UART and ensure
+// that it is enabled
+void uart_config_and_enable(size_t line) {
+  uint32_t baud_ival, baud_fval;
+  uint32_t flag = UART_LCRH_FEN;
 
-// 	switch (line) {
-// 		// setting baudrate to approx. 115246.09844 (best we can do); 1
-// stop bit 		case CONSOLE: baud_ival =   26; baud_fval = 2; break;
-// default: return;
-// 	}
+  switch (line) {
+  // setting baudrate to approx. 115246.09844 (best we can do); 1 stop bit
+  case CONSOLE:
+    baud_ival = 26;
+    baud_fval = 2;
+    break;
+  default:
+    return;
+  }
 
-// 	// line control registers should not be changed while the UART is
-// enabled, so disable it 	uint32_t cr_state = UART_REG(line, UART_CR);
-// 	UART_REG(line, UART_CR) = cr_state & ~UART_CR_UARTEN;
+  // line control registers should not be changed while the UART is enabled, so
+  // disable it
+  uint32_t cr_state       = UART_REG(line, UART_CR);
+  UART_REG(line, UART_CR) = cr_state & ~UART_CR_UARTEN;
 
-// 	// set the baud rate
-// 	UART_REG(line, UART_IBRD) = baud_ival;
-// 	UART_REG(line, UART_FBRD) = baud_fval;
+  // set the baud rate
+  UART_REG(line, UART_IBRD) = baud_ival;
+  UART_REG(line, UART_FBRD) = baud_fval;
 
-// 	// set the line control registers: 8 bit, no parity, 1 or 2 stop bits,
-// FIFOs enabled 	UART_REG(line, UART_LCRH) = UART_LCRH_WLEN_HIGH |
-// UART_LCRH_WLEN_LOW | flag;
+  // set the line control registers: 8 bit, no parity, 1 or 2 stop bits, FIFOs
+  // enabled
+  UART_REG(line, UART_LCRH) = UART_LCRH_WLEN_HIGH | UART_LCRH_WLEN_LOW | flag;
 
-// 	// re-enable the UART; enable both transmit and receive regardless of
-// previous state 	UART_REG(line, UART_CR) = cr_state | UART_CR_UARTEN |
-// UART_CR_TXE | UART_CR_RXE;
-// }
-
-char uart_getc(size_t line) {
-  while (UART_REG(line, UART_FR) & UART_FR_RXFE)
-    ; // wait for data ready
-  return UART_REG(line, UART_DR);
+  // re-enable the UART; enable both transmit and receive regardless of previous
+  // state
+  UART_REG(line, UART_CR) =
+      cr_state | UART_CR_UARTEN | UART_CR_TXE | UART_CR_RXE;
 }
 
-void uart_putc(size_t line, char c) {
-  while (UART_REG(line, UART_FR) & UART_FR_TXFF)
-    ; // wait for room in buffer
-  UART_REG(line, UART_DR) = c;
+char UARTNB::getc() { return UART_REG(line, UART_DR); }
+
+bool UARTNB::can_receive_io() {
+  return !(UART_REG(line, UART_FR) & UART_FR_RXFE);
 }
 
-void uart_putl(size_t line, const char *buf, size_t blen) {
+void UARTNB::putc(char c) { tx_buffer.push(c); }
+
+void UARTNB::putl(const char *buf, size_t blen) {
   for (size_t i = 0; i < blen; i++) {
-    uart_putc(line, *(buf + i));
+    tx_buffer.push(buf[i]);
   }
 }
 
-void uart_puts(size_t line, const char *buf) {
+void UARTNB::puts(const char *buf) {
   while (*buf) {
-    uart_putc(line, *buf);
+    tx_buffer.push(*buf);
     buf++;
   }
 }
 
-void uart_printf(size_t line, const char *fmt, ...) {
+void UARTNB::printf(const char *fmt, ...) {
   va_list va;
   char ch, buf[12];
 
   va_start(va, fmt);
   while ((ch = *(fmt++))) {
     if (ch != '%')
-      uart_putc(line, ch);
+      putc(ch);
     else {
       ch = *(fmt++);
       switch (ch) {
       case 'u':
         ui2a(va_arg(va, unsigned int), 10, buf);
-        uart_puts(line, buf);
+        puts(buf);
         break;
       case 'd':
         i2a(va_arg(va, int), buf);
-        uart_puts(line, buf);
+        puts(buf);
         break;
       case 'x':
         ui2a(va_arg(va, unsigned int), 16, buf);
-        uart_puts(line, buf);
+        puts(buf);
         break;
       case 's':
-        uart_puts(line, va_arg(va, char *));
+        puts(va_arg(va, char *));
         break;
       case 'c':
-        uart_putc(line, va_arg(va, int));
+        putc(va_arg(va, int));
         break;
       case '%':
-        uart_putc(line, ch);
+        putc(ch);
         break;
       case '\0':
         return;
@@ -128,4 +133,15 @@ void uart_printf(size_t line, const char *fmt, ...) {
     }
   }
   va_end(va);
+}
+
+void UARTNB::send_io() {
+  if (tx_buffer.is_empty()) {
+    return;
+  }
+
+  if (!(UART_REG(line, UART_FR) & UART_FR_TXFF)) {
+    UART_REG(line, UART_DR) = tx_buffer.peek();
+    tx_buffer.pop();
+  }
 }
