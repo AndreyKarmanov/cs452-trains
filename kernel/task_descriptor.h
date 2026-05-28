d8739444bff70f348ae073ab764a96db7a560943
Andrey <Andrey@Karmanov.ca>
2026-05-28 14:47:57 -0400

    factor out defines from headers into main

--- kernel/task_descriptor.h
+++ kernel/task_descriptor.h
@@ -2,9 +2,6 @@
 
 #include <cstdint>
 
-#define TASK_STACK_SIZE 4096
-#define TASK_DESCRIPTORS 4
-
 enum class TaskStatus { READY, RUNNING, TERMINATED };
 
 struct TaskDescriptor {
@@ -12,9 +9,5 @@ struct TaskDescriptor {
   int parent_tid;
   int priority;
   TaskStatus state;
-  uint64_t sp_el0;   // this is the stack pointer
-  uint64_t elr_el1;  // this is where to jump after kernel code is done
-  uint64_t spsr_el1; // Program stat, including condition codes, interrupt
-
-  uint8_t *stack_base;
+  uint64_t sp_el0;
 };
