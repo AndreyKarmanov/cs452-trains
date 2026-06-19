44fc4ec55fd1a05069ce7101a6870fcce7bbf9e6
Andrey Karmanov <andrey.karmanov@uwaterloo.ca>
2026-06-19 16:12:43 -0400

    static string upgrade

--- kernel/static_string.h
+++ kernel/static_string.h
@@ -1,12 +1,43 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 
 template <size_t SIZE> struct StaticString {
   char data[SIZE]{};
   size_t len{0};
 
+private:
+  bool append_unsigned(uint64_t value) {
+    char buffer[32];
+    size_t index = 0;
+
+    do {
+      buffer[index++]  = static_cast<char>('0' + (value % 10));
+      value           /= 10;
+    } while (value != 0 && index < sizeof(buffer));
+
+    if (value != 0)
+      return false;
+
+    while (index > 0) {
+      if (!append(buffer[--index]))
+        return false;
+    }
+    return true;
+  }
+
+  bool append_signed(int64_t value) {
+    if (value < 0) {
+      if (!append('-'))
+        return false;
+      return append_unsigned(static_cast<uint64_t>(-(value + 1)) + 1);
+    }
+    return append_unsigned(static_cast<uint64_t>(value));
+  }
+
+public:
   StaticString() = default;
   StaticString(const char *str) {
     while (len < SIZE - 1 && str[len] != '\0') {
@@ -23,6 +54,77 @@ template <size_t SIZE> struct StaticString {
     }
     data[len] = '\0';
   }
+
+  template <typename... Args> StaticString(const Args &...args) {
+    set(args...);
+  }
+
+  void clear() {
+    len     = 0;
+    data[0] = '\0';
+  }
+
+  bool empty() const { return len == 0; }
+
+  bool pop_back() {
+    if (len == 0)
+      return false;
+    --len;
+    data[len] = '\0';
+    return true;
+  }
+
+  template <typename... Args> void set(const Args &...args) {
+    clear();
+    (append(args), ...);
+  }
+
+  bool append(char ch) {
+    if (len >= SIZE - 1)
+      return false;
+    data[len++] = ch;
+    data[len]   = '\0';
+    return true;
+  }
+
+  bool append(const char *str) {
+    while (*str != '\0') {
+      if (!append(*str++))
+        return false;
+    }
+    return true;
+  }
+
+  template <size_t OTHER_SIZE>
+  bool append(const StaticString<OTHER_SIZE> &other) {
+    return append(other.data);
+  }
+
+  bool append(unsigned int value) { return append_unsigned(value); }
+
+  bool append(unsigned short value) { return append_unsigned(value); }
+
+  bool append(unsigned char value) { return append_unsigned(value); }
+
+  bool append(unsigned long value) { return append_unsigned(value); }
+
+  bool append(unsigned long long value) { return append_unsigned(value); }
+
+  bool append(int value) { return append_signed(value); }
+
+  bool append(short value) { return append_signed(value); }
+
+  bool append(signed char value) { return append_signed(value); }
+
+  bool append(long value) { return append_signed(value); }
+
+  bool append(long long value) { return append_signed(value); }
+
+  template <typename... Args> bool append(const Args &...args) {
+    return (append(args) && ...);
+  }
+
+  const char *c_str() const { return data; }
 
   constexpr bool operator==(const StaticString &other) const {
     if (len != other.len)
