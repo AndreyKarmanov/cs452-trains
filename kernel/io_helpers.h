0e75a758a4b542853a37c0baeb948f81a914c151
Andrey Karmanov <andrey.karmanov@uwaterloo.ca>
2026-06-23 11:08:21 -0400

    update io helpers

--- kernel/io_helpers.h
+++ kernel/io_helpers.h
@@ -12,6 +12,18 @@ template <size_t SIZE> int Puts(int tid, const StaticString<SIZE> &str) {
   return Puts(tid, str.c_str());
 }
 
+template <typename... Args> int Debug_Puts(int tid, const Args &...args) {
+  static constexpr int DEBUG_LINE = 40;
+  static int lines                = 0;
+
+  StaticString<TX::MAX_DATA_LENGTH> str;
+  str.append("\033[s\033[", DEBUG_LINE + lines, ";1H");
+  str.append(args...);
+  str.append("\033[u");
+  lines++;
+  return Puts(tid, str);
+}
+
 template <typename... Args> int Puts(int tid, const Args &...args) {
   StaticString<TX::MAX_DATA_LENGTH> str;
   str.set(args...);
