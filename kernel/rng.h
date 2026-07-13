954383960cfe8793b249ec8c95a251e918ecba32
Andrey Karmanov <andrey.karmanov@uwaterloo.ca>
2026-07-13 12:50:51 -0400

    fix dtype

--- kernel/rng.h
+++ kernel/rng.h
@@ -7,33 +7,33 @@
 
 class RNG {
   // https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf
-  uint32_t state = 42;
-  uint32_t a     = 438293613;
-  uint32_t c     = 1;       // must be odd for full period with def params
-  uint32_t m     = 1 << 30; // 2^30, assume power of 2
+  uint64_t state = 42;
+  uint64_t a     = 438293613;
+  uint64_t c     = 1;       // must be odd for full period with def params
+  uint64_t m     = 1 << 30; // 2^30, assume power of 2
 
 public:
   RNG() = default;
-  RNG(uint32_t seed) : state(seed) {};
-  RNG(uint32_t seed, uint32_t a, uint32_t c, uint32_t m_pow)
+  RNG(uint64_t seed) : state(seed) {};
+  RNG(uint64_t seed, uint64_t a, uint64_t c, uint64_t m_pow)
       : state(seed), a(a), c(c), m(1 << m_pow) {
           // could check that a,c,m guarantee best period but need primes and
           // stuff and easier to do offline.
         };
-  uint32_t nextNum() {
+  uint64_t nextNum() {
     state = (a * state + c) & (m - 1);
     return state;
   };
 };
 
 class Unif : public RNG {
-  uint32_t min;
-  uint32_t max;
+  uint64_t min;
+  uint64_t max;
 
 public:
-  Unif(uint32_t seed, uint32_t min, uint32_t max)
+  Unif(uint64_t seed, uint64_t min, uint64_t max)
       : RNG(seed), min(min), max(max) {};
-  uint32_t nextNum() { return min + (RNG::nextNum() % (max - min + 1)); };
+  uint64_t nextNum() { return min + (RNG::nextNum() % (max - min + 1)); };
 };
 
 inline bool test_rng() {
@@ -55,7 +55,7 @@ inline bool test_rng() {
     counts[n - MIN_VAL]++;
   }
 
-  for (int i = MIN_VAL; i < MAX_VAL; i++) {
+  for (int i = MIN_VAL - 1; i < MAX_VAL; i++) {
     Debug_Puts(txs_tid, "Num ", i + 1, " count: ", counts[i]);
   }
 
